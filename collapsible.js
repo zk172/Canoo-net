c186a7353119fd8f803237a653c84a4149acd5b7
ZAK <zk.app67@gmail.com>
2025-12-22 20:12:28 +0000

    modify collapsible js code to include arrows, add arrows to sidebar

--- collapsible.js
+++ collapsible.js
@@ -1,14 +1,33 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
+// imgElem.style.marginLeft = '-15px';  
+path1 = 'images/layout/menuarrowright.gif';
+path2 = 'images/layout/menuarrowdown.gif';
+
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    var imgElem = document.createElement("img");
     if (content.style.display === "block") {
+      if(this.parentElement.firstElementChild.tagName == "IMG") this.previousElementSibling.remove();
+      
+      imgElem.src = path1;
+      this.parentElement.insertBefore(imgElem,this);
+
       content.style.display = "none";
-    } else {
+      
+    } 
+
+    else {
+      if(this.parentElement.firstElementChild.tagName == "IMG") this.previousElementSibling.remove();
+      
+      imgElem.src = path2;
+      this.parentElement.insertBefore(imgElem,this);
       content.style.display = "block";
+
+      
     }
   });
 }
