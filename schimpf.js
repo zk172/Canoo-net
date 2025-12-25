75c3b750df328d17ee5c9c45052c6451a8ea7e19
ZAK <zk.app67@gmail.com>
2025-12-25 15:24:41 +0000

    initial commit of mittelfeld pages

---
+++ schimpf.js
@@ -0,0 +1,61 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+function isSchimpf(input) {
+    return (input.search("ögel") != -1) ||
+        (input.search("%f6gel") != -1) ||
+        (input.search("%F6gel") != -1) ||
+        (input.search("arsch") != -1) ||
+        (input.search("bums") != -1) ||
+        (input.search("voegel") != -1) ||
+        (input.search("fotz") != -1) ||
+        (input.search("fick") != -1) ||
+        (input.search("fuck") != -1) ||
+        (input.search("penis") != -1) ||
+        (input.search("titt") != -1) ||
+        (input.search("sex") != -1) ||
+        (input.search("vagina") != -1) ||
+        (input.search("nippel") != -1) ||
+        (input.search("wichs") != -1) ||
+        (input.search("nutte") != -1) ||
+        (input.search("hure") != -1) ||
+        (input.search("anal") != -1) ||
+        (input.search("schwuchtel") != -1) ||
+        (input.search("dildo") != -1) ||
+        (input.search("eichel") != -1) ||
+        (input.search("sex") != -1) ||
+        (input.search("moese") != -1) ||
+        (input.search("moepse") != -1) ||
+        (input.search("%f6pse") != -1) ||
+        (input.search("%F6ps") != -1) ||
+        (input.search("m%f6se") != -1) ||
+        (input.search("m%F6se") != -1) ||
+        (input.search("möse") != -1);
+}
+}
+/*
+     FILE ARCHIVED ON 08:39:22 Jul 29, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:17:57 Dec 25, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.747
+  exclusion.robots: 0.021
+  exclusion.robots.policy: 0.009
+  esindex: 0.011
+  cdx.remote: 12.07
+  LoadShardBlock: 154.831 (6)
+  PetaboxLoader3.datanode: 154.68 (7)
+  load_resource: 47.53
+  PetaboxLoader3.resolve: 36.633
+*/
