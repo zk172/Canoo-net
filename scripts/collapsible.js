ddef7fee62ec7a0be4ea715fbe80cf7d401ee8c4
ZAK <zk.app67@gmail.com>
2026-07-25 18:48:25 -0700

    put arrows inside button, place arrows next to text

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -1,40 +1,32 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-// imgElem.style.marginLeft = '-15px';  
-path1 = 'images/layout/menuarrowright.gif';
-path2 = 'images/layout/menuarrowdown.gif';
-
+var path1 = "images/layout/menuarrowright.gif";
+var path2 = "images/layout/menuarrowdown.gif";
 
 function imageCollapse() {
+    this.classList.toggle("active");
 
-  this.classList.toggle("active");
     var content = this.nextElementSibling;
-    var imgElem = document.createElement("img");
+    var imgElem = this.querySelector("img");
+
     if (content.style.display === "block" || content.style.display === "") {
-      if(this.previousElementSibling.tagName == "IMG") this.previousElementSibling.remove();
-      
-      imgElem.src = path1;
-      this.parentElement.insertBefore(imgElem,this);
-
-      content.style.display = "none";
-      
-    } 
-
-    else {
-      if(this.previousElementSibling.tagName == "IMG") this.previousElementSibling.remove();
-      
-      imgElem.src = path2;
-      this.parentElement.insertBefore(imgElem,this);
-      content.style.display = "block";
-      
+        imgElem.src = path1;
+        content.style.display = "none";
+    } else {
+        imgElem.src = path2;
+        content.style.display = "block";
     }
-  
 }
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", imageCollapse)
-  coll[i].nextElementSibling.style.display="none";
-  coll[i].previousElementSibling.src=path1;
+    coll[i].addEventListener("click", imageCollapse);
 
+    coll[i].nextElementSibling.style.display = "none";
+
+    var imgElem = coll[i].querySelector("img");
+
+    if (imgElem) {
+        imgElem.src = path1;
+    }
 }
